a8653fe5a388380aa0a907a909549cefe93f3367
birdycoder <349281162@qq.com>
2019-03-05 23:52:43 +0100

    Update TerrainGenerator.cpp

--- RealtimeProceduralTerrainGeneration/RealtimeProceduralTerrainGeneration/TerrainGenerator.cpp
+++ RealtimeProceduralTerrainGeneration/RealtimeProceduralTerrainGeneration/TerrainGenerator.cpp
@@ -374,6 +374,7 @@ double lerp(double t, double a, double b) {
 	return a + t * (b - a);
 }
 
+//perlin version to calculate the dot product of a randomly selected gradient vector and the 8 location vectors
 double grad(int hash, double x, double y, double z) {
 	int h = hash & 15;
 	// Convert lower 4 bits of hash into 12 gradient directions
@@ -381,6 +382,31 @@ double grad(int hash, double x, double y, double z) {
 		v = h < 4 ? y : h == 12 || h == 14 ? x : z;
 	return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
 }
+/* more understandable version to calculate the dot product
+double grad(int hash, double x, double y, double z)
+{
+    switch(hash & 0xF)
+    {
+        case 0x0: return  x + y;
+        case 0x1: return -x + y;
+        case 0x2: return  x - y;
+        case 0x3: return -x - y;
+        case 0x4: return  x + z;
+        case 0x5: return -x + z;
+        case 0x6: return  x - z;
+        case 0x7: return -x - z;
+        case 0x8: return  y + z;
+        case 0x9: return -y + z;
+        case 0xA: return  y - z;
+        case 0xB: return -y - z;
+        case 0xC: return  y + x;
+        case 0xD: return -y + z;
+        case 0xE: return  y - x;
+        case 0xF: return -y - z;
+        default: return 0; // never happens
+    }
+}
+*/
 
 double CTerrainGenerator::noise(double x, double y, double z) {
 
